b6ece0c8e7f2fc170e9c05c16ef1dc5e6e15b67b
LilianFiori <lilianfiori@live.com>
2016-10-13 22:53:52 -0300

    dia 13/10

--- Banco.sql
+++ Banco.sql
@@ -1,38 +1,18 @@
-﻿create table estados(
-EstadoId integer NOT NULL,
-Nome varchar(50) NOT NULL DEFAULT '',
-Sigla char(2) NOT NULL DEFAULT '',
-CONSTRAINT PK_Estados PRIMARY KEY(EstadoID)
-);
-
-create table cidades(
-CidadeId integer NOT NULL,
-EstadoId integer NOT NULL,
-Nome varchar(50) NOT NULL DEFAULT '',
-CONSTRAINT PK_Cidades PRIMARY KEY(CidadeID),
-CONSTRAINT FK_Estados_Cidades FOREIGN KEY (EstadoId)
-REFERENCES Estados (EstadoId)
-);
-
-create table usuario(
+﻿create table usuario(
 IdUsuario SERIAL NOT NULL,
-CidadeId integer NOT NULL,
-EstadoId integer NOT NULL,
 Nome varchar(100) NOT NULL DEFAULT '',
 Cpf varchar(15) NOT NULL DEFAULT '',
 Telefone varchar(20) NOT NULL DEFAULT '',
+Cidade varchar(60) NOT NULL DEFAULT '',
+Estado varchar(65) NOT NULL DEFAULT '',
 Endereco varchar(100) NOT NULL DEFAULT '',
 Bairro varchar(100) NOT NULL DEFAULT '',
 NumeroCarteira integer NOT NULL,
 TipoCarteira varchar(100) NOT NULL DEFAULT '',
-CONSTRAINT PK_Pessoas PRIMARY KEY (IdUsuario),
-CONSTRAINT FK_Estados_Usuario FOREIGN KEY (EstadoId)
-REFERENCES Estados (EstadoId),
-CONSTRAINT FK_Cidades_Usuario FOREIGN KEY (CidadeId)
-REFERENCES Cidades (CidadeId)
+CONSTRAINT PK_Pessoas PRIMARY KEY (IdUsuario)
 );
 
-create table veiculos(
+create table veiculo(
 IdVeiculo SERIAL NOT NULL,
 TipoVeiculo varchar(50) NOT NULL DEFAULT '',
 Placa varchar(50) NOT NULL DEFAULT '',
@@ -42,4 +22,3 @@ CONSTRAINT PK_Veiculos PRIMARY KEY (IdVeiculo)
 );
 
 SELECT * FROM usuario
-
